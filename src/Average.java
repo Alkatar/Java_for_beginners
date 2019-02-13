676a5ec50446d195688dd22401a8f01f5e593fb1
Alkatar <wandering90@gmail.com>
2019-02-13 21:38:11 +0300

    Задача №5: Отработка арифметических операций , ver.1.1 - исправлен тип данных avg и avg2 - ввод данных теперь через один поток считывания

--- src/Average.java
+++ src/Average.java
@@ -5,18 +5,16 @@ public class Average {
 
         float avg;
         Scanner l = new Scanner(System.in);
-        Scanner m = new Scanner(System.in);
-        Scanner n = new Scanner(System.in);
 
-        System.out.println("Введите x, y, z:");
-        int x = l.nextInt();
-        int y = m.nextInt();
-        int z = n.nextInt();
+        System.out.println("Введите числа x, y и z через пробел и нажмите Enter:");
+        float x = l.nextInt();
+        float y = l.nextInt();
+        float z = l.nextInt();
 
         avg = (x+y+z)/3;
         System.out.printf("Average=%.3f\n", avg);
 
-        int avg2 = (x+y+z)/6;
+        float avg2 = (x+y+z)/6;
 
         if (avg2>3)
             System.out.println("Программа выполнена корректно");
