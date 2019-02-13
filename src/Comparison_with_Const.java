33200e458e0003415595f467436a0566342e77e6
Alkatar <wandering90@gmail.com>
2019-02-13 22:46:56 +0300

    Задача №6: Условие вывода данных, ver.1.0

---
+++ src/Comparison_with_Const.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class Comparison_with_Const {
+    public static void main(String[] args) {
+
+        Scanner l = new Scanner(System.in);
+
+        System.out.println("Введите число t и нажмите Enter:");
+
+        float t = l.nextFloat();
+
+        float x = 5;
+        float y = 6;
+        float z = 18;
+
+        if (t==x | t==y | t==z)
+            System.out.println("Данное значение имеется в константах");
+        else
+            System.out.println("Такой константы нет!");
+    }
+}
