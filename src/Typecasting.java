7725da13271fa158a13b10fb377eda56885e50b6
Alkatar <wandering90@gmail.com>
2019-03-11 22:37:52 +0300

    Задача №14: Преобразование типов v.1.1

--- src/Typecasting.java
+++ src/Typecasting.java
@@ -4,11 +4,11 @@ public class Typecasting {
     public static void main(String[] args) {
 
         Scanner l = new Scanner(System.in);
-        System.out.println("Введите целое число:");
+        System.out.println("Введите  число:");
         String s = l.nextLine();
 
-        int x = Integer.parseInt(s);
-        double d=x;
+        double d = Double.parseDouble(s);
+        int x=(int)d;
         System.out.println( "Число типа String:\n" + s);
         System.out.println( "Число типа int:\n" + x);
         System.out.println( "Число типа double:\n" + d);
